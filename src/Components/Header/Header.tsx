dd431159cb1c4e362b6f307eb11d45a3699d517c
Bryan Borgesano <BryanBorgesano97@gmail.com>
2023-01-15 12:22:44 -0500

    Conditionally show nav items

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,51 +1,95 @@
-import { AppBar, Toolbar, Typography, Box, Button, IconButton, Stack } from "@mui/material";
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import { Link } from "react-router-dom";
-import { useAuth } from "../../Context/AuthContext";
-import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
-import DinnerDiningIcon from "@mui/icons-material/DinnerDining";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useHeader } from "./useHeader";
+import { MainDrawer } from "../Drawer/MainDrawer";
 
 export const Header = () => {
-  const { user, logout } = useAuth();
+  const { user, logout, navigationItems } = useHeader();
+  const [open, setOpen] = React.useState<boolean>(false);
+  const theme = useTheme();
+  const tabletAndSmaller = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <Box sx={{ flexGrow: 1, textDecoration: "none" }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            component={Link}
-            to="/"
-            disableFocusRipple
-            disableRipple
-            disableTouchRipple>
-            <RestaurantMenuIcon />
-          </IconButton>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            What We Cooked
-          </Typography>
-          {user ? (
-            <Stack direction="row" spacing={2}>
-              <Button component={Link} to="/manage-meals" color="inherit" startIcon={<DinnerDiningIcon />}>
-                Manage Meals
+    <>
+      <Box sx={{ flexGrow: 1, textDecoration: "none" }}>
+        <AppBar position="static">
+          <Toolbar>
+            {tabletAndSmaller ? (
+              <Box sx={{ flexGrow: 1 }}>
+                <IconButton size="large" edge="start" color="inherit" onClick={() => setOpen(true)}>
+                  <MenuIcon />
+                </IconButton>
+              </Box>
+            ) : (
+              <>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  component={Link}
+                  to="/"
+                  disableFocusRipple
+                  disableRipple
+                  disableTouchRipple>
+                  <RestaurantMenuIcon />
+                </IconButton>
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                  What We Cooked
+                </Typography>
+              </>
+            )}
+
+            {user && !tabletAndSmaller && (
+              <Stack direction="row" spacing={2}>
+                {navigationItems.map(item => (
+                  <Button component={Link} to={item.path} color="inherit" startIcon={item?.icon}>
+                    {item.text}
+                  </Button>
+                ))}
+                <Button color="inherit" endIcon={<LogoutIcon />} onClick={() => logout()}>
+                  Logout
+                </Button>
+              </Stack>
+            )}
+            {user && tabletAndSmaller && (
+              <>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  component={Link}
+                  to="/"
+                  disableFocusRipple
+                  disableRipple
+                  disableTouchRipple>
+                  <RestaurantMenuIcon />
+                </IconButton>
+              </>
+            )}
+
+            {!user && (
+              <Button component={Link} to="/login" color="inherit">
+                Login
               </Button>
-              <Button component={Link} to="/profile" color="inherit" startIcon={<PersonIcon />}>
-                Profile
-              </Button>
-              <Button color="inherit" endIcon={<LogoutIcon />} onClick={() => logout()}>
-                Logout
-              </Button>
-            </Stack>
-          ) : (
-            <Button component={Link} to="/login" color="inherit">
-              Login
-            </Button>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
+            )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <MainDrawer open={open} onClose={() => setOpen(false)} navigationItems={navigationItems} />
+    </>
   );
 };
