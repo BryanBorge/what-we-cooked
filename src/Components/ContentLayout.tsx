f705a958a782a0d3a4a8fd1533cec03403eacb52
Bryan Borgesano <BryanBorgesano97@gmail.com>
2023-01-15 12:19:32 -0500

    Remove unused imports

--- src/Components/ContentLayout.tsx
+++ src/Components/ContentLayout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { MainRoutes } from "../Pages/Routes";
